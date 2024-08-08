e42f50d186ed6a035cb22e674b6c329362c7f028
Paweł Worzała <pawelworzala86@gmail.com>
2024-08-08 17:57:37 +0200

    auto save

--- frame/cmd.asm
+++ frame/cmd.asm
@@ -4,6 +4,8 @@
 
     include ..\include\extern.inc
 
+    include ..\include\math.asm
+
     .code
 
 	{{INIT}}
---
+++ source/math.js
@@ -0,0 +1,7 @@
+var numA = 12.2
+var numB = 1233.29
+var numC = 0.0
+function main(){
+    numC = numA * numB
+    printf('ok %f', numC)
+}
