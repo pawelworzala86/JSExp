e83a348046dcd1c84d06a8be20a0ac2609d868dd
Paweł Worzała <pawelworzala86@gmail.com>
2024-08-09 09:56:34 +0200

    auto save

---
+++ source/while.js
@@ -0,0 +1,24 @@
+
+var indexWhile = 10
+
+function tst(){
+
+    while(indexWhile!=0){
+        dec indexWhile
+        printf("%s", "OK")
+    }
+
+
+    indexWhile = 10
+    while(indexWhile!=0){
+        dec indexWhile
+        printf("%s", "OK")
+    }
+
+}
+
+function start(){
+
+    tst()
+
+}
