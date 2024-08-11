2f75c502a3bc9d328573dd7289f2569a79e70dd0
Paweł Worzała <pawelworzala86@gmail.com>
2024-08-11 10:31:24 +0200

    auto save

--- source/matrix.js
+++ source/matrix.js
@@ -3,6 +3,7 @@ var matA = NULL
 var matrixZero = 0.0
 var matrixTwo = 2.0
 var matrixOne = 1.0
+var matrixOneMinus = -1.0
 
 var matIndex = 0
 
@@ -44,28 +45,32 @@ function mat4_print(caption,mat){
 var mtan = 0.0
 var mfv = 0.0
 var fff = 0.0
+var nf = 0.0
+var nfA = 0.0
 function mat4_perspectiveNO(out,fovy,aspect,near,far){
     mfv = fovy / matrixTwo
     Macro_Math_tan(mfv, mtan)
     fff = matrixOne / mtan
-    /*out[0] = fff / aspect;
-    out[1] = 0;
-    out[2] = 0;
-    out[3] = 0;
-    out[4] = 0;
-    out[5] = f;
-    out[6] = 0;
-    out[7] = 0;
-    out[8] = 0;
-    out[9] = 0;
-    out[11] = -1;
-    out[12] = 0;
-    out[13] = 0;
-    out[15] = 0;*/
+    out[0] = fff / aspect;
+    out[1] = matrixZero;
+    out[2] = matrixZero;
+    out[3] = matrixZero
+    out[4] = matrixZero
+    out[5] = fff;
+    out[6] = matrixZero
+    out[7] = matrixZero
+    out[8] = matrixZero
+    out[9] = matrixZero
+    out[11] = matrixOneMinus;
+    out[12] = matrixZero
+    out[13] = matrixZero
+    out[15] = matrixZero
     //if (far != null && far !== Infinity) {
-      //var nf = 1 / (near - far);
-      //out[10] = (far + near) * nf;
-      //out[14] = 2 * far * near * nf;
+        nfA = near - far
+      nf = matrixOne / nfA;
+      nfA = far + near
+      out[10] = nfA * nf;
+      out[14] = matrixTwo * far * near * nf;
     //} else {
     //  out[10] = -1;
     //  out[14] = -2 * near;
@@ -74,13 +79,20 @@ function mat4_perspectiveNO(out,fovy,aspect,near,far){
   }
 
 
+var fovy = 45.0
+var aspect = 1.0
+var near = 0.1
+var far = 1000.0
+
 function main(){
 
     mat4_create(matA)
 
     mat4_identity(matA)
-
     mat4_print('mat4_identity',matA)
+
+    mat4_perspectiveNO(matA,fovy,aspect,near,far)
+    mat4_print('mat4_perspectiveNO',matA)
 
     printf('ok')
 }
