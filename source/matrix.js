efcf64795fd72ed1e27268892c1fff5c9bbcb65d
Paweł Worzała <pawelworzala86@gmail.com>
2024-08-11 10:13:48 +0200

    auto save

--- source/matrix.js
+++ source/matrix.js
@@ -1,12 +1,12 @@
-var matA = [0.0,0.0,0.0,0.0, 0.0,0.0,0.0,0.0, 0.0,0.0,0.0,0.0, 0.0,0.0,0.0,0.0]
+var matA = NULL
 
 var matrixZero = 0.0
 var matrixOne = 1.0
 
 var matIndex = 0
 
-function mat4_create(){
-    rax = malloc(16*8)
+function mat4_create(out){
+    out = malloc(16*8)
     //return out
 }
 function mat4_identity(out){
@@ -37,7 +37,8 @@ function mat4_identity(out){
     out[14] = matrixZero
     out[15] = matrixOne*/
 }
-function mat4_print(mat){
+function mat4_print(caption,mat){
+    printf('%s %s',caption, lf)
     printf('%f', mat[0])
     printf(', %f', mat[1])
     printf(', %f', mat[2])
@@ -47,16 +48,27 @@ function mat4_print(mat){
     printf(', %f', mat[5])
     printf(', %f', mat[6])
     printf(', %f', mat[7])
+    printf(lf)
+    printf('%f', mat[8])
+    printf(', %f', mat[9])
+    printf(', %f', mat[10])
+    printf(', %f', mat[11])
+    printf(lf)
+    printf('%f', mat[12])
+    printf(', %f', mat[13])
+    printf(', %f', mat[14])
+    printf(', %f', mat[15])
+    printf(lf)
 }
 
 
 function main(){
 
-    //matA = mat4_create()
+    mat4_create(matA)
 
     mat4_identity(matA)
 
-    mat4_print(matA)
+    mat4_print('mat4_identity',matA)
 
     printf('ok')
 }
