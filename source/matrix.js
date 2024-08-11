ae34dabdbd48ec0e5581a95b703671ab86b21ac5
Paweł Worzała <pawelworzala86@gmail.com>
2024-08-11 10:24:46 +0200

    auto save

--- source/matrix.js
+++ source/matrix.js
@@ -1,6 +1,7 @@
 var matA = NULL
 
 var matrixZero = 0.0
+var matrixTwo = 2.0
 var matrixOne = 1.0
 
 var matIndex = 0
@@ -40,6 +41,37 @@ function mat4_print(caption,mat){
     printf(', %f', mat[15])
     printf(lf)
 }
+var mtan = 0.0
+var mfv = 0.0
+var fff = 0.0
+function mat4_perspectiveNO(out,fovy,aspect,near,far){
+    mfv = fovy / matrixTwo
+    Macro_Math_tan(mfv, mtan)
+    fff = matrixOne / mtan
+    /*out[0] = fff / aspect;
+    out[1] = 0;
+    out[2] = 0;
+    out[3] = 0;
+    out[4] = 0;
+    out[5] = f;
+    out[6] = 0;
+    out[7] = 0;
+    out[8] = 0;
+    out[9] = 0;
+    out[11] = -1;
+    out[12] = 0;
+    out[13] = 0;
+    out[15] = 0;*/
+    //if (far != null && far !== Infinity) {
+      //var nf = 1 / (near - far);
+      //out[10] = (far + near) * nf;
+      //out[14] = 2 * far * near * nf;
+    //} else {
+    //  out[10] = -1;
+    //  out[14] = -2 * near;
+    //}
+    //return out;
+  }
 
 
 function main(){
