3d65328ef9714bbc7c743140769e49ce78f885d5
Paweł Worzała <pawelworzala86@gmail.com>
2024-08-14 18:39:21 +0200

    auto save

--- source/random.js
+++ source/random.js
@@ -2,11 +2,24 @@ var sin = 0.0
 var num = 90.0
 var init = 232.7876
 var minusOne = -1.0
-function main(){
+var zero = 0.0
+
+var mixed = 342.886
+
+function random(){
     num = Math.deg2rad(init)
     sin = Math.sin(num)
-    if(sin<0.0){
-        sin = minusOne*sin
+    if(sin<zero){
+        sin = minusOne * sin
     }
-    printf('ok %f', sin)
+    init = sin * mixed
+    printf('ok %f %s', sin, lf)
+}
+
+function main(){
+    random()
+    random()
+    random()
+    random()
+    random()
 }
