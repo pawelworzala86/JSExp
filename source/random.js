1dbdac110bc4d31e6d8260e704ad90d1f9f4a14f
Paweł Worzała <pawelworzala86@gmail.com>
2024-08-14 18:35:23 +0200

    auto save

--- source/random.js
+++ source/random.js
@@ -1,7 +1,12 @@
 var sin = 0.0
 var num = 90.0
+var init = 232.7876
+var minusOne = -1.0
 function main(){
-    num = Math.deg2rad(num)
+    num = Math.deg2rad(init)
     sin = Math.sin(num)
+    if(sin<0.0){
+        sin = minusOne*sin
+    }
     printf('ok %f', sin)
 }
