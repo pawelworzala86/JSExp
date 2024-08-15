af1a4ccabadcc265124f0b7d542878d800f63fb9
Paweł Worzała <pawelworzala86@gmail.com>
2024-08-15 06:11:00 +0200

    auto save

--- file.js
+++ file.js
@@ -15,26 +15,18 @@ function addF64(arr){
         array.push(array8[i])
     }
 }
-function addI64(bigIntValue){
-    /*var array64 = new BigInt64Array(arr)
-    var array8 = new Uint8Array(array64.buffer)*/
-    let buffer = new ArrayBuffer(8);
-    let view = new DataView(buffer);
-
-    // Ustawienie wartości BigInt w DataView
-    view.setBigUint64(0, bigIntValue);
-
-    // Konwersja DataView na Uint8Array
-    let array8 = new Uint8Array(buffer);
+function addI64(arr){
+    var array64 = new BigUint64Array(arr)
+    var array8 = new Uint8Array(array64.buffer)
     //array8.map(f=>{array.push(f)})
     for(let i=0;i<array8.length;i++){
         array.push(array8[i])
     }
 }
 
-addI64(BigInt(vertices.length*8))
+addI64([BigInt(vertices.length*8)])
 addF64(vertices)
-addI64(BigInt(coords.length*8))
+addI64([BigInt(coords.length*8)])
 addF64(coords)
 
 
